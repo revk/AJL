454fb0dc58072206e798534a1035672b488feaaa
Adrian Kennard <a@k.gg>
2020-03-16 09:54:36 +0000

    Minor tweaks to review before coding actual libraries

--- ajl.h
+++ ajl.h
@@ -35,18 +35,16 @@
 // allowing a complete tree to be created just by using j_add from root with the full path of values.
 //
 // Error handling. The library tries to avoid errors.
-// An internal malloc fail will abort with err() unless a NULL can be returned, e.g. j_init()
+// An internal malloc fail will abort with err() unless a NULL can be returned, e.g. j_create()
 // internal consistency checks use assert(), which should, of course, not happen.
 // Attempting to use a NULL as a j_t is silently ignored
 // Attempt to find/get data that does not exist returns NULL, and length -1, etc
 // Attempting to set a value should always work, converting parent values to objects/arrays, extending arrays, etc, as needed
 // Other error cases are explained in comments below
-//
-// TODO. The loading/parsing of an object should have a means to know error and line/pos of error somehow
 
 typedef struct j_s *j_t;	// Point in JSON tree, i.e. a value
 
-j_t j_init();	// Allocate a new JSON object tree that is empty, ready to be added to or read in to, NULL for error
+j_t j_create();	// Allocate a new JSON object tree that is empty, ready to be added to or read in to, NULL for error
 j_t j_delete(j_t); // Delete this value (remove from parent object if not root) and all sub objects, returns NULL
 
 // Moving around the tree, these return the j_t of the new point (or NULL if does not exist)
@@ -54,13 +52,17 @@ j_t j_root(j_t);	// Return root point
 j_t j_parent(j_t);	// Parent of this point (NULL if this point is root)
 j_t j_next(j_t);	// Next in parent object or array, NULL if at end
 j_t j_prev(j_t);	// Previous in parent object or array, NULL if at start
-j_t j_find(j_t,const char *tags);	// Find object within this object by tag - NULL if any in path does not exist
-j_t j_index(j_t,int);	// Find specific point in an array - NULL if not in the array
+j_t j_first(j_t);	// First entry in an object or array (same as j_index with 0)
+j_t j_find(j_t,const char *tags);	// Find object within this object by tag/path - NULL if any in path does not exist
+j_t j_index(j_t,int);	// Find specific point in an array, or object - NULL if not in the array
 
 // Information about a point
 const char *j_tag(j_t); // The tag of this object in parent, if it is in a parent object, else NULL
+int j_pos(j_t);	// Position in parent array or object, -1 if this is the root object so not in another array/object
 const char *j_val(j_t);	// The value of this object as a string. NULL if not found. Note that a "null" string is a valid literal value
 int j_len(j_t);	// The length of this value (characters if string or number or literal), or number of entries if object or array
+
+const char *j_get(j_t,const char *tags);	// Find and get val using tags, NULL for not found
 
 // Information about data type of this point
 int j_isarray(j_t); // True if is an array
@@ -71,15 +73,17 @@ int j_istrue(j_t);	// True if is true literal
 int j_isnumber(j_t); // True if is a number
 int j_isstring(j_t); // True if is a string (i.e. quoted, note "123" is a string, 123 is a number)
 
-// Loading an object. This returns NULL if any error, otherwise the root of the loaded object/value
-j_t j_read(FILE *);	// Read object from open file
-j_t j_read_file(const char *filename); // Read object from named file
-j_t j_read_mem(char *buffer); // Read object from string in memory (NULL terminated)
+// Loading an object. This replaces value at the j_t specified, which is usually a root from j_create()
+// Returns NULL if all is well, else a malloc'd error string
+char * j_read(j_t,FILE *);	// Read object from open file
+char * j_read_file(j_t,const char *filename); // Read object from named file
+char * j_read_mem(j_t,char *buffer); // Read object from string in memory (NULL terminated)
 
 // Output an object - note this allows output of a raw value, e.g. string or number, if point specified is not an object itself
-void j_write(j_t,FILE *);
-void j_write_file(j_t,char *filename);
-void j_write_mem(j_t,char **buffer,size_t *len);
+// Returns NULL if all is well, else a malloc'd error string
+char * j_write(j_t,FILE *);
+char * j_write_file(j_t,char *filename);
+char * j_write_mem(j_t,char **buffer,size_t *len);
 
 // Changing an object/value
 void j_string(j_t,const char *);	// Simple set this value to a string (null terminated).
@@ -89,12 +93,21 @@ void j_numberf(j_t,const char *fmt,...);	// Simple set this value to a number, i
 void j_literal(j_t,const char *);	// Simple set this value to a literal, e.g. "null", "true", "false"
 void j_object(j_t); // Simple set this value to be an object if not already
 void j_array(j_t); // Simple set this value to be an array if not already
-j_t j_add(j_t,const char *tags); // Create specified tags, and return the point that is the value for that tag.
+j_t j_add(j_t,const char *tags); // Create specified tag/path, and return the point that is the value for that tag.
+j_t j_append(j_t);	// Create new point at end of array
 
-// Additional functions to combine the above...
-void j_add_string(j_t,const char *tags,const char *);	// Simple set this value to a string (null terminated).
-void j_add_stringf(j_t,const char *tags,const char *fmt,...);	// Simple set this value to a string, using printf style format
-void j_add_numberf(j_t,const char *tags,const char *fmt,...);	// Simple set this value to a number, i.e. unquoted, using printf style format
-void j_add_literal(j_t,const char *tags,const char *);	// Simple set this value to a literal, e.g. "null", "true", "false"
+// Additional functions to combine the above... Returns point for newly added value.
+j_t j_add_string(j_t,const char *tags,const char *);	// Simple set this value to a string (null terminated).
+j_t j_add_stringf(j_t,const char *tags,const char *fmt,...);	// Simple set this value to a string, using printf style format
+j_t j_add_numberf(j_t,const char *tags,const char *fmt,...);	// Simple set this value to a number, i.e. unquoted, using printf style format
+j_t j_add_literal(j_t,const char *tags,const char *);	// Simple set this value to a literal, e.g. "null", "true", "false"
 
-void j_attach(j_t,j_t);	// Replaces first value with the second in a tree, removing second from its parent if it has one
+// Additional functions to combine the above... Returns point for newly added value.
+j_t j_append_string(j_t,const char *tags,const char *);	// Simple set this value to a string (null terminated).
+j_t j_append_stringf(j_t,const char *tags,const char *fmt,...);	// Simple set this value to a string, using printf style format
+j_t j_append_numberf(j_t,const char *tags,const char *fmt,...);	// Simple set this value to a number, i.e. unquoted, using printf style format
+j_t j_append_literal(j_t,const char *tags,const char *);	// Simple set this value to a literal, e.g. "null", "true", "false"
+
+// Moving parts of objects...
+j_t j_attach(j_t,j_t);	// Replaces first value with the second in a tree, removing second from its parent if it has one, returns first arg or NULL for error
+
--- ajlparse.h
+++ ajlparse.h
@@ -20,23 +20,33 @@
        You should have received a copy of the GNU General Public License
        along with this program.  If not, see <http://www.gnu.org/licenses/>.
      */
+
+// This used for low level parsing, and not manipulating JSON objects in memory
+// You probably want to look at ajl.h
+
 #pragma once
 #include <stdio.h>
+
+// This library provides simple means to parse underlying JSON elements, and to output them
+// Error handling is by means of setting an error state which stops further processing
+// The error string is returned from most functions, and the line/character of the error can be checked
 
 // Types
 typedef struct ajl_s *ajl_t;    // The JSON parse control structure
 
 // Those functions returning const char * return NULL for OK, else return error message. Once a parse error is found it is latched
 
-const char *ajl_close (ajl_t);  // Close control structure (closes file). For generate_mem, this sets buffer and len correctly and adds a NULL after len.
+// Common functtions
+const char *ajl_close (ajl_t);  // Close control structure (closes file). For write_mem, this sets buffer and len correctly and adds a NULL after len.
+ajl_t ajl_delete(ajl_t);	// Free the handle, returns NULL
 const char *ajl_ok (ajl_t);     // Return if error set in JSON object, or NULL if not error
-unsigned int ajl_line(ajl_t);	// Return current line number in source
-
-// Parse
 
 // Allocate control structure for parsing, from file or from memory
-ajl_t ajl_parse (FILE *);
-ajl_t ajl_parse_mem (unsigned char *buffer, size_t len);
+ajl_t ajl_read (FILE *);
+ajl_t ajl_read_file (const char *filename);
+ajl_t ajl_read_mem (unsigned char *buffer, size_t len);
+int ajl_line(ajl_t);	// Return current line number in source
+int ajl_char(ajl_t);	// Return current character position in source
 
 typedef enum
 {                               // Parse types
@@ -48,27 +58,22 @@ typedef enum
    AJL_BOOLEAN,
    AJL_NULL,
    AJL_CLOSE,                   // End of object or array
-   AJL_OBJECT,                  // Start of array
+   AJL_OBJECT,                  // Start of object
    AJL_ARRAY,                   // Start of array
 } ajl_type_t;
 
-ajl_type_t ajl_next (ajl_t, unsigned char **tag, unsigned char **value);        // Consume next syntactic unit, set tag and value to start of respective parts and return type (as above)
-
-// Process the unsigned char* value from above in to other types
-const char *ajl_number (unsigned char *, long long *);
-const char *ajl_real (unsigned char *, long double *);
-const char *ajl_boolean (unsigned char *, unsigned char *);
-const char *ajl_string (unsigned char *j, FILE *);
-const char *ajl_string_mem (unsigned char *j, char **buffer, size_t *len);      // Allocates memory for string
-
-int ajl_strcmp (const unsigned char *ajl, const unsigned char *string);
-int ajl_strcasecmp (const unsigned char *ajl, const unsigned char *string);
+// The basic parsing function consumes next element, and returns a type as above
+// If the element is within an object, then the tag is parsed and mallocd and stored in tag
+// The value of the element is parsed, and malloced and stored in value (a null is appended, not included in len)
+// The length of the value is stored in len - this is mainly to allow for strings that contain a null
+ajl_type_t ajl_parse (ajl_t, unsigned char **tag, unsigned char **value,size_t *len);
 
 // Generate
 
 // Allocate control structure for generating, to file or to memory
-ajl_t ajl_generate (FILE *);
-ajl_t ajl_generate_mem (unsigned char **buffer, size_t *len);
+ajl_t ajl_write (FILE *);
+ajl_t ajl_write_file (const char *filename);
+ajl_t ajl_write_mem (unsigned char **buffer, size_t *len);
 
 const char *ajl_add (ajl_t, const unsigned char *tag, const unsigned char *value);      // Add pre-formatted value (expects quotes, escapes, etc)
 const char *ajl_add_string (ajl_t, const unsigned char *tag, const unsigned char *value, size_t len);   // Note len=-1 means use strlen(value)
@@ -81,3 +86,4 @@ const char *ajl_add_null (ajl_t, const unsigned char *tag);
 const char *ajl_add_object (ajl_t, const unsigned char *tag);   // Start an object
 const char *ajl_add_array (ajl_t, const unsigned char *tag);    // Start an array
 const char *ajl_add_close (ajl_t);      // close current array or object
+
