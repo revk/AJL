707c2e6eccf0449ded5902554eef73577997a2d0
Adrian Kennard <a@k.gg>
2020-03-21 12:40:28 +0000

    extra functionn

--- ajlparse.c
+++ ajlparse.c
@@ -333,6 +333,11 @@ ajl_level (ajl_t j)
    return j->level;
 }
 
+int ajl_isobject(ajl_t j)
+{
+   return j->flags[j->level] & OBJECT;
+}
+
 // The basic parsing function consumes next element, and returns a type as above
 // If the element is within an object, then the tag is parsed and mallocd and stored in tag
 // The value of the element is parsed, and malloced and stored in value (a null is appended, not included in len)
