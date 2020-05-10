abeca3d2140963ebbd8a1e337d21e0863c88df52
Adrian Kennard <a@k.gg>
2020-05-10 12:15:09 +0100

    Curl version

---
+++ ajlcurl.h
@@ -0,0 +1,27 @@
+// ==========================================================================
+//
+//                       Adrian's JSON Library - ajlcurl.h
+//
+// ==========================================================================
+
+    /*
+       Copyright (C) 2020-2020  RevK and Andrews & Arnold Ltd
+
+       This program is free software: you can redistribute it and/or modify
+       it under the terms of the GNU General Public License as published by
+       the Free Software Foundation, either version 3 of the License, or
+       (at your option) any later version.
+
+       This program is distributed in the hope that it will be useful,
+       but WITHOUT ANY WARRANTY; without even the implied warranty of
+       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+       GNU General Public License for more details.
+
+       You should have received a copy of the GNU General Public License
+       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+#pragma once
+#include <ajl.h>
+#include <curl/curl.h>
+
+j_t j_curl(CURL *, j_t, const char *url, ...); // Submit curl, get curl response
